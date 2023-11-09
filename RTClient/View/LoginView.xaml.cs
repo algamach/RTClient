59b1a2816876b6e5e3ef8129a4762b5228686536
algamach <123144931+algamach@users.noreply.github.com>
2023-11-09 11:51:02 +0300

    First Login View

--- RTClient/View/LoginView.xaml.cs
+++ RTClient/View/LoginView.xaml.cs
@@ -29,5 +29,30 @@ namespace RTClient.View
             if (e.LeftButton == MouseButtonState.Pressed)
                 DragMove();
         }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAltLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
